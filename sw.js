8a14eed9676094b05cdfc1c6861c960fd44fefdd
ckcgoodnews-oss <ckc.good.news@gmail.com>
2026-08-21 14:18:36 -0400

    Update app with dynamic weekly options and management tab

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-const CACHE_NAME = 'spiritual-tracker-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
-});
+self.addEventListener('install', e => e.waitUntil(caches.open('v2').then(c => c.addAll(['./', './index.html'])))); self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
