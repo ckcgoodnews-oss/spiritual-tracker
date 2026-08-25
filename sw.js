cd28a13590652aff8d00a9cbb7ffd08911c66023
charlesfphillips <cphillips@aardvark-enterprises.net>
2026-08-25 07:41:25 -0400

    Add secure login-first tracker and cloud features

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
-self.addEventListener('install', e => e.waitUntil(caches.open('v6').then(c => c.addAll(['./', './index.html'])))); self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'spiritual-tracker-v5';
+const APP_SHELL = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
